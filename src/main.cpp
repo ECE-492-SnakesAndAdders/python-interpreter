83d76c0b8e55fd2dbe18e00e64349d85344672f7
Isaac Joffe <isaac11joffe@gmail.com>
2024-01-30 15:21:25 -0700

    initial structure

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,24 @@
 #include "main.h"
 
 
+// TODO: choose how data is stored and transferred, affecting all function signatures
+
+
+/**
+ * \brief Produces the main REPL behavior of the interpreter.
+ * \return 0 on success, another integer on failure.
+ */
 int main() {
     xpd_puts("Hello, World!");
+    // REPL: Read-Eval-Print-Loop
+    while (true) {
+        // read in user input (command / code)
+        read();
+        // evaluate and execute input received
+        eval();
+        // display the correct output from this input
+        print();
+        // continue looping in this cycle
+    }
     return 0;
 }
